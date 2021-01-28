4adf0b62b6722ce8ee6553b84454103cd0d755b9
qcod3r <77826626+qcod3r@users.noreply.github.com>
2021-01-28 23:32:17 +0300

    Update turkscout.sh

--- TurkScout/turkscout.sh
+++ TurkScout/turkscout.sh
@@ -16,7 +16,7 @@ menu () {
 read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Which tool do you want to use? \e[0m\en' tool
 if [[ $tool == 1 ]]; then
 cd tools/durshikax
-./durshikax.sh
+bash durshikax.sh
 
 elif [[ $tool == 2 ]]; then
 cd tools/senior-mask
